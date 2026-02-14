c649f868f0b98f2ebfc34f75bfba16749d57dfe1
Anusuya-SK <anusuyak98.Ak@gmail.com>
2026-02-14 16:19:24 +0530

    JSX element

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-        "div",
-        { id: "child1" },
-        [React.createElement("h1", {}, "It's h1 tag"), React.createElement("h2", {}, "It's h2 tag")]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [React.createElement("h1", {}, "It's h1 tag"), React.createElement("h2", {}, "It's h2 tag")]
-    )]
-);
+// React Element - JS object - HTMLElement(render)
+//const parent = React.createElement("h1", {id: "heading"}, "This is h1 tag");
+
+// JSX (Transpiled before it reaches the JS) - parcel - babel
+// JSX - React Element - JS object - HTMLElement(render)
+const parent = <h1 id="heading">This is JSX heading</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
