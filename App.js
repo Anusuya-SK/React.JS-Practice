f3379aae76331a4977e7193e51a0c24e7e75d9d7
Anusuya-SK <anusuyak98.Ak@gmail.com>
2026-02-16 19:12:14 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Component
+// React Element
+const title = (
+    <h2>
+        This is Title
+    </h2>
+);
+
+
+// React Component
 const Parent = () => (
     <h1 className="heading" tabIndex="1">
         This is Parent heading
     </h1>
 );
 
+// Component Composition
 const Child = () => (
     <div id="container">
+        {title}
         <Parent />
         <h1>This is Child Heading</h1>
     </div>
